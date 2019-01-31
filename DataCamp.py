f6864b2251b9b5225ce6b32d9dfe7d9b2a615906
Victor Naidu <victor@vnaidu.com>
2019-01-30 19:06:46 -0800

    Add Function to Retrieve a DataCamp User's Profile Page

--- DataCamp.py
+++ DataCamp.py
@@ -1,9 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+# %matplotlib inline
 
-import requests
-from bs4 import BeautifulSoup
+def dl_profile(user):
+    url = "https://www.datacamp.com/profile/" + user
+    page = urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup
